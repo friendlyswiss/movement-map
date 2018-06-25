6eef08b5dd9d48e1ac2d116859bb857ba77484c7
friendlyswiss <cparledge@gmail.com>
2018-06-24 22:01:47 -0400

    Adds models for geodata

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'imports/get_moves'
+
+  get 'imports/unmarshall'
+
   get 'pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
